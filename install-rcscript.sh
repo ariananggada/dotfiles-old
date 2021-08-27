bbdf3e47876436391307bee500a16562be896a15
ariananggada <arian.anggada@gmail.com>
2021-08-27 17:36:19 +0700

    escaping [ char in install-rcscript.sh script

--- install-rcscript.sh
+++ install-rcscript.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env sh
 
-if ! grep -q "if [ -f \$HOME/.initrc ]" "$HOME/.bashrc"; then
+if grep -q "if \[ -f \$HOME/.initrc \]" "$HOME/.bashrc"; then
+  echo "already exists";
 
+else
   cat >> $HOME/.bashrc <<-EOF
 
-  # custom init rc load
-  if [ -f \$HOME/.initrc ]; then
-      . \$HOME/initrc
-  fi
+# custom init rc load
+if [ -f \$HOME/.initrc ]; then
+    . \$HOME/initrc
+fi
 
-  if [ -f \$HOME/.functionrc ]; then
-      . \$HOME/functionrc
-  fi
+if [ -f \$HOME/.functionrc ]; then
+    . \$HOME/functionrc
+fi
 
-  if [ -f \$HOME/.aliasrc ]; then
-      . \$HOME/.aliasrc
-  fi
+if [ -f \$HOME/.aliasrc ]; then
+    . \$HOME/.aliasrc
+fi
 
 EOF
 
