a2f35a8f9f41dc2704230a2fe371e319c6b8e739
ariananggada <arian.anggada@gmail.com>
2021-12-13 01:08:02 +0700

    update

--- scripts/macos/key-repeat-rate.sh
+++ scripts/macos/key-repeat-rate.sh
@@ -3,5 +3,5 @@
 # initialkeyrepeat (int 10 = 150ms, int 1 = 15ms)
 # keyrepeat (int 1 = 15ms)
 
-defaults write -g ApplePressAndHoldEnabled -bool false && defaults write -g InitialKeyRepeat -int 10 && defaults write -g KeyRepeat -int 1
-# defaults write -g ApplePressAndHoldEnabled -bool false && defaults write -g InitialKeyRepeat -int 15 && defaults write -g KeyRepeat -int 2
+# defaults write -g ApplePressAndHoldEnabled -bool false && defaults write -g InitialKeyRepeat -int 10 && defaults write -g KeyRepeat -int 1
+defaults write -g ApplePressAndHoldEnabled -bool false && defaults write -g InitialKeyRepeat -int 15 && defaults write -g KeyRepeat -int 2
